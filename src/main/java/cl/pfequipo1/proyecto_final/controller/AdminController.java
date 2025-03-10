e29e4102b083ca254196ee3eca956556953919bd
mjtBootcamp <bootcamp.javascript.fullstack@gmail.com>
2025-03-10 15:23:08 -0300

    Documentacion Usuario y Compañia

--- src/main/java/cl/pfequipo1/proyecto_final/controller/AdminController.java
+++ src/main/java/cl/pfequipo1/proyecto_final/controller/AdminController.java
@@ -4,6 +4,10 @@ import cl.pfequipo1.proyecto_final.dto.AdminDTO;
 import cl.pfequipo1.proyecto_final.service.AdminServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import java.util.List;
 
@@ -13,6 +17,17 @@ public class AdminController {
 
     @Autowired
     private AdminServiceImpl adminService;
+
+    @Operation(
+            summary = "Obtener el Administrador",
+            description = "Retorna los detalles del Administrador",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Administrador encontrado",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = AdminDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Administrador no encontrado")
+            }
+    )
 
     @GetMapping
     public List<AdminDTO> findAll() {
--- src/main/java/cl/pfequipo1/proyecto_final/dto/AdminDTO.java
+++ src/main/java/cl/pfequipo1/proyecto_final/dto/AdminDTO.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Modelo de Administrador")
 public class AdminDTO {
+    @Schema(description = "Nombre de Usuario del Administrador")
     private String username;
+
+    @Schema(description = "Rol del Administrador")
     private String role;
 }
