9838d9077e34cc5022925029c443934aad10c484
mjtBootcamp <bootcamp.javascript.fullstack@gmail.com>
2025-03-10 15:59:08 -0300

    Documentacion Locacion y Compañia

--- src/main/java/cl/pfequipo1/proyecto_final/dto/LocationDTO.java
+++ src/main/java/cl/pfequipo1/proyecto_final/dto/LocationDTO.java
@@ -4,17 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Modelo de Locacion")
 public class LocationDTO {
+    @Schema(description = "Id de Locacion")
     private Integer locationId;
+    @Schema(description = "Nombre de Locacion")
     private String locationName;
+    @Schema(description = "Pais de Locacion")
     private String locationCountry;
+    @Schema(description = "Ciudad de Locacion")
     private String locationCity;
+    @Schema(description = "Meta de Locacion")
     private String locationMeta;
+    @Schema(description = "Id de Compañia")
     private Integer companyId; // Para saber a qué compañía pertenece
 
     public LocationDTO(Integer locationId, String locationName, String locationCountry, String locationCity, String locationMeta) {
